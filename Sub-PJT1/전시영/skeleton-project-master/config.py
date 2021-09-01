87da141e0456897252938684852d61b5e33619c9
전시영 <nnaoww@naver.com>
2021-09-01 17:02:18 +0900

    req.2-1
    Former-commit-id: 758701d79a6be5b347891ceeb3099c3327b13343

--- Sub-PJT1/전시영/skeleton-project-master/config.py
+++ Sub-PJT1/전시영/skeleton-project-master/config.py
@@ -1,6 +1,12 @@
 import argparse
 
 # Req. 2-1	Config.py 파일 생성
+parser = argparse.ArgumentParser()
 
 # 캡션 데이터가 있는 파일 경로 (예시)
 parser.add_argument('--caption_file_path', type=str, default='.\\datasets\\captions.csv')
+
+#이미지 파일들이 저장된 경로
+parser.add_argument('--image_file_path', default='.\\datasets\\images')
+
+args=parser.parse_args()
