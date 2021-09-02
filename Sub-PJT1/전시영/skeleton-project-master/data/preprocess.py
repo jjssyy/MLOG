6a8f18347b023291a7ea04cbf40f07a6c2fa9291
전시영 <nnaoww@naver.com>
2021-09-03 00:14:08 +0900

    req 3-1
    Former-commit-id: be79075d5afc585d3f9ef4126bcb3cdcb8559852

--- Sub-PJT1/전시영/skeleton-project-master/data/preprocess.py
+++ Sub-PJT1/전시영/skeleton-project-master/data/preprocess.py
@@ -1,11 +1,18 @@
 import os
 import csv
 import numpy as np
-
+import config
+import pandas as pd
 
 # Req. 3-1	이미지 경로 및 캡션 불러오기
 def get_path_caption():
-    pass
+    # image_name| comment_number| comment
+    # 1000092795.jpg| 0| Two young guys with shaggy hair look at their hands while hanging out in the yard .
+    data= pd.read_csv(config.args.caption_file_path, encoding='cp949', error_bad_lines=False, sep='|')
+    # print(data)
+    img_paths = data['image_name']
+    captions = data[' comment']
+    return img_paths, captions
 
 
 # Req. 3-2	전체 데이터셋을 분리해 저장하기
