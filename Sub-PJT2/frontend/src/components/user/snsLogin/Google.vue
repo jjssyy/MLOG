4ee8b6eba856f316b85c67826f7a9a284894dc19
전시영 <nnaoww@naver.com>
2021-09-16 17:36:13 +0900

    [S05P21C104-80][FE] feat : 구글 로그인 추가
    Former-commit-id: 583a5c414912f7fa2938fd91bbcd09b9486e163a [formerly ae41ebf0bcb23b9e3ddfb494fe1e24f00836c11b]
    Former-commit-id: 5f680b8254c2749916ab8191fbfb396d3a9222c0

--- Sub-PJT2/frontend/src/components/user/snsLogin/Google.vue
+++ Sub-PJT2/frontend/src/components/user/snsLogin/Google.vue
@@ -1,11 +1,48 @@
 <template>
-    <div class="google-login">
-        <div class="g-logo">
-            <img src="@/assets/images/g_icon.png" alt="" width="33">
-        </div>
+  <div class="google-login">
+    <div class="g-logo">
+      <!-- <img
+        src="@/assets/images/g_icon.png"
+        alt=""
+        width="33"
+        @click="onGoogleLogin"
+      /> -->
+      <GoogleLogin
+        :params="params"
+        :renderParams="renderParams"
+        :onSuccess="onSuccess"
+        :onFailure="onFailure"
+      ></GoogleLogin>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+import GoogleLogin from "vue-google-login";
+export default {
+  components: {
+    GoogleLogin,
+  },
+  data() {
+    return {
+      params: {
+        client_id:
+          "512592128492-b88aomr2gk1n6ivkbs8h2t0lc04e97ng.apps.googleusercontent.com",
+        ux_mode: "popup",
+        redirect_uri: "http://localhost:8081/",
+      },
+      renderParams: {
+        width: 50,
+        height: 50,
+        longtitle: false,
+      },
+    };
+  },
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser.getBasicProfile());
+    },
+    onFailure() {},
+  },
+};
 </script>
