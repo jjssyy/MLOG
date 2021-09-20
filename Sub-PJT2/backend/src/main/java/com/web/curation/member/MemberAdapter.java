a812ae26a76cf0d9962dbbc2a9b2e2bde6406a1f
양동현 <mevolicus@gmail.com>
2021-09-20 18:41:22 +0900

    [S05P21C104-78][BE] feat : 회원가입 기능
    Former-commit-id: b402fc234b3dbf8e319c6da26ec3e9a1a0d34e0a [formerly cce3961b65e323d7873897bbd99214bd7371416c]
    Former-commit-id: 777e71d8b517f792889f8199b141cf0eecab486f

---
+++ Sub-PJT2/backend/src/main/java/com/web/curation/member/MemberAdapter.java
@@ -0,0 +1,15 @@
+package com.web.curation.member;
+
+public class MemberAdapter {
+    public static MemberDto entityToDto(UserProfile userProfile, UserAuth userAuth){
+        return new MemberDto.MemberDtoBuilder()
+                .uid(userAuth.getUid())
+                .email(userAuth.getEmail())
+                .emailCompany(userAuth.getEmailCompany())
+                .authToken(userAuth.getAuthToken())
+                .filePath(userProfile.getFilePath())
+                .nickname(userProfile.getNickname())
+                .hasSurveyed(userProfile.isHasSurveyed())
+                .build();
+    }
+}
