4c8d8b43b67703a08c326df8c534dd2f56e9c67f
모진혁 <gkgk246@naver.com>
2021-09-29 16:50:25 +0900

    [BE]다이어리부분 업데이트
    Former-commit-id: 4510aa1b0ccdeeb68aeb3e49f698b7624fc6805d [formerly 87cfffa6132ddbd463e1b8a5d10fbd8be7ba0ec8] [formerly 1cac63d0e265342ecd8e42c0d22f385b95917e55 [formerly 7c600220206883be4b274b57d94c0379888186dd]]
    Former-commit-id: e13a316c374e2205453a3e8f1827c0eedb771f11 [formerly d1d8e49be5e5c221ac15dba683637013298a1c0a]
    Former-commit-id: e99de5b73bed8f85d2d8121041f705ad5efdf1e8

--- Sub-PJT2/backend/src/main/java/com/web/curation/diary/DiaryService.java
+++ Sub-PJT2/backend/src/main/java/com/web/curation/diary/DiaryService.java
@@ -1,7 +1,15 @@
 package com.web.curation.diary;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 import com.web.curation.member.MemberAuthDao;
@@ -32,7 +40,9 @@ public class DiaryService {
 		}
 		return result;
 	}
-	public void WriteDiary(String Uid,String content,LocalDate date,float[] emotion) {
+	public void WriteDiary(String Uid,String content,LocalDate date) {
+		float[] emotion=new float[5];
+		emotion=find(content);
 		
 		Diary diary_info=Diary.builder()
 				.uid(Uid)
@@ -50,5 +60,60 @@ public class DiaryService {
 				.fear(emotion[4])
 				.build();
 		diaryAnalyticsDao.save(diaryAnalytics);
+	}
+	
+	
+	private float[] find(String content) {
+		double[] tmp=new double[5];
+		float[] result=new float[5];
+		JSONObject data=new JSONObject();
+		data.put("content", content);
+//		Flask호출부분
+		HttpURLConnection conn=null;
+		String reqURL= "http://172.30.1.45:5000/predict";
+		try {
+			URL url = new URL(reqURL);
+			conn=(HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json; utf-8");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setDoOutput(true);//OutputStream을 사용하여 post body 데이터 전송
+			//http 요청 실시
+			conn.connect();
+			System.out.println("http 요청 방식 : "+"POST BODY JSON");
+			System.out.println("http 요청 타입 : "+"application/json");
+			System.out.println("http 요청 주소 : "+reqURL);
+			System.out.println("http 요청 데이터 : "+data);
+			System.out.println("");
+			//Post방식으로 스트링을통한 JSON전송
+			BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			bw.write(data.toString());
+			bw.flush();
+			bw.close();
+			//서버에서 보낸 응답 데이터 수신 받기
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String returnMsg=in.readLine();
+			System.out.println("응답메세지: "+returnMsg);
+			
+			JSONParser parser =new JSONParser();
+			Object obj=parser.parse(returnMsg);
+			JSONObject result_data=(JSONObject) obj;
+			
+			tmp[0]=(double) result_data.get("neutral");
+			tmp[1]=(double) result_data.get("joy");
+			tmp[2]=(double) result_data.get("sadness");
+			tmp[3]=(double) result_data.get("anger");
+			tmp[4]=(double) result_data.get("fear");
+			result[0]=(float)tmp[0];
+			result[1]=(float)tmp[1];
+			result[2]=(float)tmp[2];
+			result[3]=(float)tmp[3];
+			result[4]=(float)tmp[4];
+			return result;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
