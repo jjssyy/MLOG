a2cb89484fa351bbd396a8e14c08a8a2e82053fe
jihyeonsongb <dhqtl1@naver.com>
2021-09-30 13:54:25 +0900

    [S05P21C104-118][FE] feat : 프로필 디자인 완료
    Former-commit-id: 812cd74a8914480308698c924528301cd8d2e3bd [formerly 5a6f6f60c7a59400400849d46052c782a312bca8] [formerly 8dd5f0bd7d7987b92c6174f7b41e896feaef2fb9 [formerly a9f06e4f63aac0a6be21c05e3ee71ff72a2f3ad3]]
    Former-commit-id: 586cb23b1f92c15ba26794b2e41ad7b7fc4cfe6c [formerly 9ea535562bd6ade5124f3e1d659a10040b552d17]
    Former-commit-id: edbe52dbfccf95b816af218bca73f0741481d6bd

--- Sub-PJT2/frontend/src/views/profile/Profile.vue
+++ Sub-PJT2/frontend/src/views/profile/Profile.vue
@@ -1,16 +1,63 @@
 <template>
-  <div>
-    <h2 @click="logout">로그아웃</h2>
+  <div class="profile">
+    <div class="profile-header2">
+      <button class="back">
+        <i class="fas fa-arrow-left fa-2x"></i>
+      </button>
+      <div class="userinfo">
+        <div id="header-img">
+          <div>
+            <img
+              :src="filePath"
+              alt=""
+              width="100%"
+              height="100%"
+              style="border-radius: 50%;"
+            />
+          </div>
+        </div>
+        <h2 class="nickname">{{ nickname }} 님</h2>
+      </div>
+      <h3 class="logout" @click="logout">로그아웃</h3>
+    </div>
+    <div class="profile-contents">
+      <div class="task">
+        <h1>일기장</h1>
+        <div class="subtask">
+          <h2><i class="fas fa-book"></i>내 일기장</h2>
+          <h2><i class="fas fa-music"></i>내 플레이리스트</h2>
+        </div>
+      </div>
+      <div class="task">
+        <h1>분석</h1>
+        <div class="subtask">
+          <h2><i class="fas fa-chart-bar"></i>내 일기 보고서</h2>
+          <h2><i class="fas fa-chart-pie"></i>긍·부정 보고서</h2>
+        </div>
+      </div>
+      <div class="task2">
+        <h1>설정</h1>
+        <div class="subtask">
+          <h2><i class="fas fa-cog"></i>내 정보</h2>
+          <h2><i class="fas fa-undo-alt"></i>설문조사 초기화</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   methods: {
     logout() {
       this.$store.dispatch('logout')
       this.$router.push({ name: 'Login' })
     },
+  },
+  computed: {
+    ...mapState(['filePath', 'nickname']),
   },
 }
 </script>
