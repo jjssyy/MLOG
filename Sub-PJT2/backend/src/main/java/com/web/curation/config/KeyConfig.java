a977884c8e2f5c68792435ab41353eaac11c90c2
양동현 <mevolicus@gmail.com>
2021-10-01 14:18:00 +0900

    [S05P21C104-67][BE] feat : 구글 api key 저장
    Former-commit-id: 8b35b72a6b26286f1e5e909ca8baa01600521a85 [formerly 9ccaf97cd2632692d8ae32ff7cf7ace9febeb244] [formerly 6590dc22ab77ded3e4c923adfd634733b93317c4 [formerly 848d301ce77bba6016ee2a8f69d75bf3ec372625]]
    Former-commit-id: 696be39d077baaa39fda374a5bf6bb98d218f0a6 [formerly 34bdb4d92cac7bfb7220aed611359a16c236c4cb]
    Former-commit-id: 88629c271b642bb695501594a53c3053b6bc5cc6

---
+++ Sub-PJT2/backend/src/main/java/com/web/curation/config/KeyConfig.java
@@ -0,0 +1,18 @@
+package com.web.curation.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@RequiredArgsConstructor
+@PropertySource("classpath:application-API-KEY.properties")
+public class KeyConfig {
+
+    private final Environment environment;
+
+    public String getGCPKey(){
+        return environment.getProperty("GCP-API-KEY");
+    }
+}
