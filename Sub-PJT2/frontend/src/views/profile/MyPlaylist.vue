05f06a6cd1356cfd317eb94aa033cf542a486158
jihyeonsongb <dhqtl1@naver.com>
2021-10-04 14:55:43 +0900

    [S05P21C104-129][FE] feat : 프로필 - 내 플레이 리스트 디자인, 유튜브 연결
    Former-commit-id: 5d4d84e222f8f977b9ab2d04282b2f13bbc6a8bd [formerly 759a414be1dcfa0426ab2943c20ae5672f0b20ed] [formerly f0e9688a9295f56be90fcd67485f727aed006ecf [formerly 921174ced38aa0210e584019926c8f92e5842f83]]
    Former-commit-id: 24e8f30ce78c8d0c993fd13c6389d23cd4f7b202 [formerly 5ca8fc261dc19561031c40368674d4651b4ffb25]
    Former-commit-id: 6387b1cee480e028d8429bcdcfd1133526ba0527

--- Sub-PJT2/frontend/src/views/profile/MyPlaylist.vue
+++ Sub-PJT2/frontend/src/views/profile/MyPlaylist.vue
@@ -14,13 +14,45 @@
           </span>
         </h1>
       </div>
+      <youtube
+        id="genre-music"
+        :video-id="musicVideoId || defaultVideo"
+        :player-vars="playerVars"
+        ref="youtube"
+        style="display:none;"
+      ></youtube>
       <div class="myplay-list">
-        <MyPlaylistItem
-          v-for="(myPlay, idx) in myPlayList"
-          :key="idx"
-          :myPlay="myPlay"
-          :idx="idx"
-        />
+        <div v-for="(myPlay, idx) in myPlayList" :key="idx">
+          <div class="myplay-item">
+            <div class="myplay-item-date">
+              {{ myPlay | customDate }}
+            </div>
+            <div class="myplay-item-box">
+              <div class="myplay-item-contents">
+                <div @click="goDiary(idx)">
+                  <h2 style="cursor: pointer;">
+                    {{ myPlay.musicTitle }}
+                  </h2>
+                  <p style="cursor: pointer;">
+                    {{ myPlay.musicArtist }}
+                  </p>
+                </div>
+                <div>
+                  <span v-show="playingIdx != idx">
+                    <button class="myplay-btn" @click="playVideo(idx)">
+                      <i class="fas fa-play"></i>
+                    </button>
+                  </span>
+                  <span v-show="playingIdx == idx">
+                    <button class="myplay-btn" @click="stopVideo()">
+                      <i class="fas fa-pause"></i>
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,58 +62,17 @@
 import '@/assets/css/views/mydiary.scss'
 import { getMyPlaylist } from '@/api/ProfileApi.js'
 import { mapState } from 'vuex'
-import MyPlaylistItem from '@/components/profile/MyPlaylistItem.vue'
 
 export default {
-  components: {
-    MyPlaylistItem,
-  },
   data() {
     return {
-      myPlayList: [
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-        {
-          date: '20210910',
-          musicTitle: '바라만본다',
-          musicArtist: 'MSG워너비(MOM)',
-          videoId: 'rM5JjNfPHA',
-        },
-      ],
+      playerVars: {
+        autoplay: 1,
+      },
+      musicVideoId: '',
+      defaultVideo: '',
+      playingIdx: -1,
+      myPlayList: [],
     }
   },
   async created() {
@@ -96,9 +87,39 @@ export default {
     goProfile() {
       this.$router.push({ name: 'Profile' })
     },
+    goDiary(idx) {
+      this.$router.push({
+        name: 'ReadDiary',
+        params: { date: this.myPlayList[idx].date },
+      })
+    },
+    async playVideo(idx) {
+      this.musicVideoId = this.myPlayList[idx].videoId
+      this.playingIdx = idx
+      await this.player.playVideo()
+    },
+    async stopVideo() {
+      this.playingIdx = -1
+      await this.player.stopVideo()
+    },
   },
   computed: {
+    player() {
+      return this.$refs.youtube.player
+    },
     ...mapState(['uid']),
+  },
+  filters: {
+    customDate(myPlay) {
+      return (
+        myPlay.date.substring(0, 4) +
+        '년 ' +
+        myPlay.date.substring(4, 6) +
+        '월 ' +
+        myPlay.date.substring(6, 8) +
+        '일 '
+      )
+    },
   },
 }
 </script>
