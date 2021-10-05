e70c816825260d6db4022a6299b25a32320a0685
양동현 <mevolicus@gmail.com>
2021-10-05 14:44:10 +0900

    [S05P21C104-151][BE] refactor : 백엔드 리펙토링
    Former-commit-id: ef9197b14b45d99e62a229e9ec1f2066f24ec4a2 [formerly ba3ef3c25af32faa65986f393afe18ec25c8df46] [formerly 0f7d5eec951794ec29a9c775656e2c69cc60639e [formerly 936c18d02a7b5b00810e8eee96273ab339a6bc79]]
    Former-commit-id: 774e6b418f8f1f78f93ed037a689752d46b91517 [formerly ff831f1f34a0ed952d88c0e0a5c16b0d57df8823]
    Former-commit-id: 29568e46a01f370f58ba4c9d3b12f6cf030061b2

--- Sub-PJT2/backend/src/main/java/com/web/curation/WebCurationApplication.java
+++ Sub-PJT2/backend/src/main/java/com/web/curation/WebCurationApplication.java
@@ -2,12 +2,15 @@ package com.web.curation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class WebCurationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebCurationApplication.class, args);
+		SpringApplication app = new SpringApplication(WebCurationApplication.class);
+		app.addListeners(new ApplicationPidFileWriter()); // pid 파일을 생성하는 writer 등록
+		app.run(args);
 	}
 
 }
