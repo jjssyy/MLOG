8fce8ec8b8ab8e250ae8b6ed4f8f69673fc4e037
양동현 <mevolicus@gmail.com>
2021-10-05 14:38:07 +0900

    [S05P21C104-150][FE] refactor : 배포 관련 리펙토링
    Former-commit-id: 9a5a89e466e83c61441d47b9d6cde917a72b76c7 [formerly 109f31156cbda250b5026fab7bc53db09b1382b7] [formerly 267995385f344e916f6b30a88c46a9f7314fdbdb [formerly d76cbba8776f0e3adf5bc8a5b6aa9742b6806520]]
    Former-commit-id: e23adba425e1f698f61aab9d16aed2203b858f71 [formerly 7fd9752be07d8f8c967c7c5726a7198bfef57992]
    Former-commit-id: 1781414be63a8996df445404c37b4977d14f094a

--- Sub-PJT2/frontend/src/util/http-common.js
+++ Sub-PJT2/frontend/src/util/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8080/',
+  // baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.REMOTE_API_URL,
   headers: {
     'Content-type': 'application/json',
   },
--- Sub-PJT2/frontend/vue.config.js
+++ Sub-PJT2/frontend/vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  devServer: {
-    overlay: false,
+  configureWebpack: {
+    // other webpack options to merge in ...
   },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: { host: '0.0.0.0', hot: true, disableHostCheck: true },
 }
