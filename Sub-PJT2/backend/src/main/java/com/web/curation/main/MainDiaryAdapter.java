1be2bd2884fa66bcd486427b6f6a91409f8a6190
전시영 <nnaoww@naver.com>
2021-10-06 18:34:59 +0900

    [S05P21C104-136][BE]fix : 데이터타입 변경
    Former-commit-id: c3d056449f9393d8f4f3829304c0462bd3484ecd [formerly 1431d569aaf599363e70ed8adc16c64ef2393d1a] [formerly 6fec35d23a8704f90cc6d2df6324aab712f98cdf [formerly d917af426c46dec195b4e925a71a880da229fd53]]
    Former-commit-id: fa731f6f46d54f6e381728e46fc755c6c9ec1062 [formerly b547f393e341aef475c29f0286a3ef457e02c3e7]
    Former-commit-id: 31c996f8e0cf7a8f3925079d37e36c7d6fa1698d

--- Sub-PJT2/backend/src/main/java/com/web/curation/main/MainDiaryAdapter.java
+++ Sub-PJT2/backend/src/main/java/com/web/curation/main/MainDiaryAdapter.java
@@ -7,7 +7,7 @@ import com.web.curation.music.MusicInfo;
 public class MainDiaryAdapter {
     public static MainDiaryDto entityToDto(Diary diary, DiaryAnalyticsSentiment diaryAnalyticsSentiment, MusicInfo musicInfo){
         return new MainDiaryDto.MainDiaryDtoBuilder()
-                .date(diary.getDiaryDate())
+                .date(diary.getDiaryDate().toString())
                 .sentiment(diaryAnalyticsSentiment.getSentiment())
                 .accuracy(diaryAnalyticsSentiment.getAccuracy())
                 .music_title(musicInfo.getMusicTitle())
