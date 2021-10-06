d553ff5df31da35c84ef46cd2f81eda50f207bdb
양동현 <mevolicus@gmail.com>
2021-10-06 09:23:48 +0000

    Update Kakao.vue
    Former-commit-id: e50c17f451cc309c9bb87a4840de113b3bbd9880 [formerly ff574465501cf11d29b81b838eeb92690eea4690] [formerly cbbce5de76ac779d0c39be5949d4f09024329bf9 [formerly 15231e81f9e493a898765dd3f202b74143a97364]]
    Former-commit-id: 5aa09aa0c1bbeb447fb6ba14c579781369d533e6 [formerly 37ba40db43c4617a482c34c8431deb6da344f0d5]
    Former-commit-id: b70554a3ca92f6c708c30eb4adae96f4114a85f1

--- Sub-PJT2/frontend/src/components/user/snsLogin/Kakao.vue
+++ Sub-PJT2/frontend/src/components/user/snsLogin/Kakao.vue
@@ -43,6 +43,12 @@ export default {
         },
         (err) => {
           console.log(err);
+          console.log(err.status);
+          if(err.status == 409){
+            window.location.replace(
+        "https://kauth.kakao.com/oauth/authorize?client_id=2e422a8e10fb191d40ff2b6fb7439b41&redirect_uri=http://j5c104.p.ssafy.io&response_type=code&scope=account_email"
+      );
+          }
         }
       );
     }
