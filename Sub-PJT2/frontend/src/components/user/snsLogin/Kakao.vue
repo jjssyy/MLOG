31ea939b2eef231467bc83e064e4a93c99c197bf
양동현 <mevolicus@gmail.com>
2021-10-07 02:36:03 +0000

    Update Kakao.vue
    Former-commit-id: 3059fb5b477d67d6cfc96a7ba3b6cebdafc8dddf [formerly f08eb69abed8654d0ee6df228d21dc716de9c72c] [formerly 0bf1120df3ca849498317c59c35d4e56e7d43a0f [formerly 24ea41065e7f1b12e083c903fe01f3acdd41f9f4]]
    Former-commit-id: 94a0dfd7de8625a958b4b4338abaf3de05750603 [formerly ea75e4578e1fc95d50980174c629f5f84bf40aa5]
    Former-commit-id: f4d0945ce3ef7e4ae94e7bc1d79095c24dd29b62

--- Sub-PJT2/frontend/src/components/user/snsLogin/Kakao.vue
+++ Sub-PJT2/frontend/src/components/user/snsLogin/Kakao.vue
@@ -46,9 +46,9 @@ export default {
           console.log(err.response.status)
           if (err.response.status == 409) {
             alert('이메일을 사용 허가해주세요')
-                  window.location.replace(
-              "https://kauth.kakao.com/oauth/authorize?client_id=2e422a8e10fb191d40ff2b6fb7439b41&redirect_uri=http://j5c104.p.ssafy.io&response_type=code&scope=account_email"
-            );
+              //     window.location.replace(
+              // "https://kauth.kakao.com/oauth/authorize?client_id=2e422a8e10fb191d40ff2b6fb7439b41&redirect_uri=http://j5c104.p.ssafy.io&response_type=code&scope=account_email"
+            // );
           }
         },
       )
